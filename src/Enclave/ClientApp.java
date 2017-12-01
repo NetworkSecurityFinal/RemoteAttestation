1290fa9bf6bcb7c854c9c6afa5a8ba4ee7e41a8b
BenjaminKargul <BenjaminKargul@gmail.com>
2017-12-01 16:38:13 -0700

    Added 128 bit hardware keys

--- src/Enclave/ClientApp.java
+++ src/Enclave/ClientApp.java
@@ -2,11 +2,14 @@ package Enclave;
 
 public class ClientApp {
 
-	/*
-	 * This is the client application
-	 * Jen and Kenzie, your code will go here
-	 * 
-	 * To create an enclave, we made the EnclaveManagement class to simplify the process of creation
-	 * It has a get enclave method, and you can use that to grab any info you need
-	 */
+ /*
+  * This is the client application
+  * Jen and Kenzie, your code will go here
+  * 
+  * To create an enclave, we made the EnclaveManagement class to simplify the process of creation
+  * It has a get enclave method, and you can use that to grab any info you need
+  */
+  public static void main(String[] args){
+    EnclaveManagement em = new EnclaveManagement();
+  }
 }

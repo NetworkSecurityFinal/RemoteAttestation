78c16f56ccbb77b649cd8c92e809abf32da6ada5
Madeline MacDonald <madeline.macd@gmail.com>
2017-12-01 20:00:25 -0700

    Enhanced enclave functionality, added support for messages

--- src/Enclave/Enclave.java
+++ src/Enclave/Enclave.java
@@ -2,28 +2,52 @@ package enclave;
 
 import java.math.BigInteger;
 import java.security.*;
+
+import javax.crypto.KeyAgreement;
+import javax.crypto.interfaces.DHPublicKey;
 import javax.crypto.spec.*;
 
+/**
+ * Software simulation of an SGX enclave
+ * that exposes a subset of the SGX instruction set used for remote attestation
+ * @author Madeline MacDonald
+ * @since 12/1/17
+ * @version beta 1.0
+ */
 public class Enclave {
-
-	private int eid;
-	private String FileName;
-	private PublicKey spPublicKey;
+	
+	//Hold the p and g values for the DHKE
 	private static DHParameterSpec paramSpec;
-
+	//These are hardware keys
+	//in a real enclave these would have been generated when the enclave was created
+	//These are kept hidden from the manufacturer (Intel) and used to generate other provisioning keys
 	static byte[] sealing_key = new byte[16];
 	static byte[] provisioning_key = new byte[16];
+	
+	private int eid;
+	private String FileName;
+	private PublicKey spPublicKey;	
+	private KeyPair dhKeyPair;
+	private KeyAgreement dhKeyAgree;
 
 	static {
+		
+		//generate keys in a static block 
+		//to simulate their creation when the hardware was made
 		SecureRandom sr = new SecureRandom();
 		sr.nextBytes(provisioning_key);
 		sr.nextBytes(sealing_key);
 		
+		//Init P and G for Diffie Hellman
 		paramSpec = new DHParameterSpec(new BigInteger("47"), new BigInteger("71"));
 	}
 
 	/**
-	 * Creates a new enclave
+	 * Creates a new instance of an enclave simulator 
+	 * that exposes a subset of the SGX instruction set used for remote attestation.
+	 * This is equivalent to sgx_create_enclave()
+	 * @param enclaveFileName
+	 * @param enclaveID
 	 */
 	public Enclave(String enclaveFileName, int enclaveID) {
 		// Make the choice to not load enclave file
@@ -33,37 +57,54 @@ public class Enclave {
 	}
 	
 	/*
-	 * Kenzie, Jen, these functions are for initializing PSE
-	 * This should include TPM work
-	 * These need to be called before and after sgx_ra_init
+	 * These functions are for initializing PSE, which is where TPM logic would be called
+	 * These need to be called before and after sgx_ra_init()
 	 */
 	public void sgx_create_pse_session() {}
 	public void sgx_close_pse_session() {}
 	
-	/*
-	 * Accepts public key from Service Provider
-	 * Create a context for DHKE, returns the DH public key
+	/**
+	 * Initializes the enclave for remote attestation.
+	 * Stores the service provider's public key, and initializes a Diffie Hellman key exchange
+	 * @param spPubKey The service provider's public key
+	 * @return the public Diffie Hellman key
 	 */
 	public byte[] sgx_ra_init(PublicKey spPubKey)
-	{
-		spPublicKey = spPubKey;
-		//takes public key from SP
-		//Creates context for DHKE
+	{	
+		try {
+			spPublicKey = spPubKey;
+			KeyPairGenerator kpg = KeyPairGenerator.getInstance("DiffieHellman");
+			kpg.initialize(paramSpec);
+			dhKeyPair = kpg.generateKeyPair();
+			dhKeyAgree = KeyAgreement.getInstance("DiffieHellman");
+			dhKeyAgree.init(dhKeyPair.getPrivate());
+			
+			byte[] dhPubKeyBytes = ((DHPublicKey) dhKeyPair.getPublic()).getY().toByteArray();
+			return dhPubKeyBytes;
+		} catch (NoSuchAlgorithmException e) {
+			System.out.println(e.getMessage());
+		} catch (InvalidAlgorithmParameterException e) {
+			System.out.println(e.getMessage());
+		} catch (InvalidKeyException e) {
+			System.out.println(e.getMessage());
+		}
 		return new byte[16];
 	}
 	
-	/*
-	 * retrieves the extended group ID, 
-	 * an anonymous signature scheme used for identification
+	/**
+	 * retrieves the extended group ID, which indicates the attestation service provider
+	 * 0 indicates Intel's attestation service, != 0 indicates a third party attestation service
+	 * 
+	 * This simulated enclave follows Intel's attestation protocol
+	 * @return extended group id
 	 */
-	public byte[] sgx_get_extended_epid_group_id()
+	public int sgx_get_extended_epid_group_id()
 	{
-		return new byte[16];
+		return 0;
 	}
 	
-	/*
-	 * Should return a msg1 object
-	 * Just contains an object containing the public DH key
+	/**
+	 * 
 	 */
 	public void sgx_ra_get_msg1()
 	{
